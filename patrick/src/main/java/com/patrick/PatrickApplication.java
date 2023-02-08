177d23f6a4bf55e67742e6c1eb9e9913d859e1e9
Pablog <pablo.foucher1@gmail.com>
2023-02-08 08:38:45 +0100

    Initialization

---
+++ patrick/src/main/java/com/patrick/PatrickApplication.java
@@ -0,0 +1,13 @@
+package com.patrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrickApplication.class, args);
+	}
+
+}
