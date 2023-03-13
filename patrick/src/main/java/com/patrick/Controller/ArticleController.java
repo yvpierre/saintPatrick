4adc777f55bf5f129daaed6accb3b7e92fe77262
Pablog <pablo.foucher1@gmail.com>
2023-03-13 14:36:55 +0100

    CRUD

--- patrick/src/main/java/com/patrick/Controller/ArticleController.java
+++ patrick/src/main/java/com/patrick/Controller/ArticleController.java
@@ -2,11 +2,10 @@ package com.patrick.Controller;
 
 import com.patrick.Entity.Article;
 import com.patrick.Entity.Category;
+import com.patrick.Entity.Command;
 import com.patrick.Repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -18,6 +17,16 @@ public class ArticleController {
     @Autowired
     ArticleRepository articleRepo ;
 
+    //==================================================================================================
+    //CREATE
+    @RequestMapping(path="/create")
+    public void create(@RequestBody Article article) {
+        articleRepo.save(article);
+    }
+    //==================================================================================================
+
+    //==================================================================================================
+    //GET
     @RequestMapping(path="/get")
     public Article get(@RequestParam long id) {
         Optional<Article> article = articleRepo.findById(id) ;
@@ -46,4 +55,39 @@ public class ArticleController {
 
         return articlesFiltered ;
     }
+    //==================================================================================================
+
+    //==================================================================================================
+    //UPDATE
+    @RequestMapping(path="/update")
+    public boolean update(@PathVariable Long id, @RequestBody Article article) {
+        Optional<Article> articleOptional = articleRepo.findById(id);
+        if (articleOptional.isPresent()) {
+            //Modification des champs
+            Article updateArticle = articleOptional.get();
+            updateArticle.setName(article.getName());
+            updateArticle.setDescription(article.getDescription());
+            updateArticle.setPrice(article.getPrice());
+            updateArticle.setQuantity(article.getQuantity());
+            updateArticle.setCategories(article.getCategories());
+            updateArticle.setImgUrl(article.getImgUrl());
+            updateArticle.setIsMajor(article.getIsMajor());
+            updateArticle.setSize(article.getSize());
+            updateArticle.setStockDispo(article.isStockDispo());
+            //sauvegarde des modifications
+            articleRepo.save(updateArticle);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //==================================================================================================
+
+    //==================================================================================================
+    //DELETE
+    @RequestMapping(path="/delete")
+    public void delete(@RequestParam long id) {
+        articleRepo.deleteById(id);
+    }
+    //==================================================================================================
 }
--- patrick/src/main/java/com/patrick/Controller/CommandController.java
+++ patrick/src/main/java/com/patrick/Controller/CommandController.java
@@ -6,6 +6,8 @@ import com.patrick.Entity.Command;
 import com.patrick.Repository.ArticleRepository;
 import com.patrick.Repository.CommandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,6 +23,33 @@ public class CommandController {
     @Autowired
     ArticleRepository articleRepo;
 
+    //==================================================================================================
+    //CREATE
+    @PostMapping(path="/create")
+    public boolean create(@RequestBody Command command) {
+        if(command.getArticles().size() > 0){
+            //on vérifie que les articles en question existent bien
+            for(Article article : command.getArticles()){
+                Optional<Article> articleFromDb = articleRepo.findById(article.getId()) ;
+                if(!articleFromDb.isPresent())
+                    return false;
+            }
+            //ensuite on leur retire 1 de quantité
+            for(Article article : command.getArticles()){
+                Optional<Article> articleFromDb = articleRepo.findById(article.getId()) ;
+                articleFromDb.get().setQuantity(articleFromDb.get().getQuantity() - 1);
+                articleRepo.save(articleFromDb.get());
+            }
+            commandRepo.save(command);
+            return true;
+        }
+        else
+            return false;
+    }
+    //==================================================================================================
+
+    //==================================================================================================
+    //GET
     @RequestMapping(path="/get")
     public Command get(@RequestParam long id) {
         Optional<Command> command = commandRepo.findById(id) ;
@@ -47,6 +76,29 @@ public class CommandController {
 
         return commandsFiltered ;
     }
+    //==================================================================================================
+
+    //==================================================================================================
+    //UPDATE
+    @PutMapping("/{id}")
+    public boolean updateCommand(@PathVariable Long id, @RequestBody Command command) {
+        Optional<Command> commandOptional = commandRepo.findById(id);
+        if (commandOptional.isPresent()) {
+            //Modification des champs
+            Command updatedCommand = commandOptional.get();
+            updatedCommand.setBuyerName(command.getBuyerName());
+            updatedCommand.setBuyerEmail(command.getBuyerEmail());
+            updatedCommand.setBuyerAddress(command.getBuyerAddress());
+            updatedCommand.setAmount(command.getAmount());
+            updatedCommand.setDelivered(command.isDelivered());
+            updatedCommand.setArticles(command.getArticles());
+            //sauvegarde des modifications
+            commandRepo.save(updatedCommand);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     @RequestMapping(path="/confirm")
     public boolean getAllFromCategory(@RequestParam long id) {
@@ -58,27 +110,19 @@ public class CommandController {
         else
             return false;
     }
+    //==================================================================================================
 
-    @PostMapping(path="/create")
-    public boolean create(@RequestBody Command command) {
-        if(command.getArticles().size() > 0){
-
-            //on vérifie que les articles en question existent bien
-            for(Article article : command.getArticles()){
-                Optional<Article> articleFromDb = articleRepo.findById(article.getId()) ;
-                if(!articleFromDb.isPresent())
-                    return false;
-            }
-            //ensuite on leur retire 1 de quantité
-            for(Article article : command.getArticles()){
-                Optional<Article> articleFromDb = articleRepo.findById(article.getId()) ;
-                articleFromDb.get().setQuantity(articleFromDb.get().getQuantity() - 1);
-                articleRepo.save(articleFromDb.get());
-            }
-            commandRepo.save(command);
+    //==================================================================================================
+    //DELETE
+    @DeleteMapping("/{id}")
+    public boolean deleteCommand(@PathVariable Long id) {
+        Optional<Command> commandOptional = commandRepo.findById(id);
+        if (commandOptional.isPresent()) {
+            commandRepo.deleteById(id);
             return true;
-        }
-        else
+        } else {
             return false;
+        }
     }
+    //==================================================================================================
 }
